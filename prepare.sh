821d2067dc33862a66d84634fbaab4ad40944a8a
Raimon7 <raimon7th@yandex.ru>
2019-08-23 14:55:27 +0300

    Update prepare.sh
    added --force-yes in upgrade section

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,7 @@
 
 apt update && apt upgrade -y
 apt install mc htop iftop iotop sysstat htop iftop iotop sysstat apt-transport-https ca-certificates curl software-properties-common nethogs  gnupg-agent lsof fail2ban -y
-apt autoremove -y && apt autoclean -y
+apt autoremove -y && apt autoclean -y --force-yes
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
