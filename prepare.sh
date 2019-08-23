718049dc2a8ce3cdfc6ecd3d9a34abccc92e4eaa
Raimon7 <raimon7th@yandex.ru>
2019-08-23 14:18:09 +0300

    Create prepare.sh
    run as root or with sudo

---
+++ prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+apt install mc htop iftop iotop sysstat htop iftop iotop sysstat apt-transport-https ca-certificates curl software-properties-common nethogs  gnupg-agent lsof fail2ban -y
+apt autoremove -y && apt autoclean -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update && apt-get -y install docker-ce docker-compose
+
+docker version
